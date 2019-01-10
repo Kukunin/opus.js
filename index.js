0d19e05015f78a8df42a792de93d88e20a61d5e3
Sergiy Kukunin <sergey.kukunin@gmail.com>
2019-01-10 13:31:25 +0200

    Wait until FastSound initialized

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const OggDemuxer  = require('./src/demuxer');
 const OpusDecoder = require('./src/decoder');
-const FastSound = require('fast-sound');
 
-module.exports = function(fastSound) {
-  return FastSound(fastSound).then(function(Opus) {
-    OpusDecoder(Opus);
-  });
+module.exports = function(config) {
+  return OpusDecoder(config);
 };
--- src/decoder.js
+++ src/decoder.js
@@ -1,19 +1,25 @@
 var AV = require('av');
+const FastSound = require('fast-sound');
 
-module.exports = function(Opus) {
+module.exports = function(config) {
   var OpusDecoder = AV.Decoder.extend(function() {
     AV.Decoder.register('opus', this);
 
     this.prototype.init = function() {
       this.buflen = 4096;
-      this.buf = Opus._malloc(this.buflen);
-
       this.outlen = 4096;
-      this.outbuf = Opus._malloc(this.outlen * this.format.channelsPerFrame * 4);
-      this.f32 = this.outbuf >> 2;
 
+      this.promise = new Promise((resolve) => {
+        FastSound(config).then((Opus) => {
 
-      this.opus = Opus._opus_decoder_create(this.format.sampleRate, this.format.channelsPerFrame, this.buf);
+          this.buf = Opus._malloc(this.buflen);
+          this.opus = Opus._opus_decoder_create(this.format.sampleRate, this.format.channelsPerFrame, this.buf);
+          this.outbuf = Opus._malloc(this.outlen * this.format.channelsPerFrame * 4);
+          this.f32 = this.outbuf >> 2;
+          Opus.then = undefined; // to avoid infinite resolving loop
+          resolve(Opus);
+        });
+      });
     };
 
     this.prototype.readChunk = function() {
@@ -24,30 +30,36 @@ module.exports = function(Opus) {
       var packet = list.first;
       list.advance();
 
-      if (this.buflen < packet.length) {
-        this.buf = opus._realloc(this.buf, packet.length);
-        this.buflen = packet.length;
-      }
+      return this.promise.then((Opus) => {
+        if (this.buflen < packet.length) {
+            this.buf = Opus._realloc(this.buf, packet.length);
+            this.buflen = packet.length;
+        }
 
-      Opus.HEAPU8.set(packet.data, this.buf);
+        Opus.HEAPU8.set(packet.data, this.buf);
 
-      var len = Opus._opus_decode_float(this.opus, this.buf, packet.length, this.outbuf, this.outlen, 0);
-      if (len < 0)
-        throw new Error("Opus decoding error: " + len);
+        var len = Opus._opus_decode_float(this.opus, this.buf, packet.length, this.outbuf, this.outlen, 0);
+        if (len < 0)
+            throw new Error("Opus decoding error: " + len);
 
-      var samples = Opus.HEAPF32.subarray(this.f32, this.f32 + len * this.format.channelsPerFrame);
-      return new Float32Array(samples);
+        var samples = Opus.HEAPF32.subarray(this.f32, this.f32 + len * this.format.channelsPerFrame);
+        return new Float32Array(samples);
+      });
     };
 
     this.prototype.destroy = function() {
       this._super();
-      Opus._free(this.buf);
-      Opus._free(this.outbuf);
-      Opus._opus_decoder_destroy(this.opus);
+
+      this.promise.then((Opus) => {
+        Opus._free(this.buf);
+        Opus._free(this.outbuf);
+        Opus._opus_decoder_destroy(this.opus);
+        this.opus = null;
+      });
+      this.promise = null;
 
       this.buf = null;
       this.outbuf = null;
-      this.opus = null;
     };
   });
 }
